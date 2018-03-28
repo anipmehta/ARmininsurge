5e375182480b9815f5c9cec011203ee4ae04c1b6
Lujia Yu <yulujia928@ufl.edu>
2018-03-28 14:58:05 -0400

    Sprint 3

---
+++ Assets/transition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transition : MonoBehaviour {
+
+    CanvasGroup Info;
+    bool trans;
+
+    // Use this for initialization
+    void Start()
+    {
+        Info = GetComponent<CanvasGroup>();
+        trans = false;
+        Info.alpha = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trans)
+        {
+            if (Info.alpha != 0)
+                Info.alpha -= (float)0.4 * Time.deltaTime;
+
+        }
+    }
+
+    public void closeInfo()
+    {
+        trans = true;
+    }
+}
---
+++ Assets/transitionafter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transitionafter : MonoBehaviour {
+    CanvasGroup Info;
+    bool trans;
+
+    // Use this for initialization
+    void Start()
+    {
+        Info = GetComponent<CanvasGroup>();
+        trans = false;
+        Info.alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trans)
+        {
+            if (Info.alpha != 1)
+                Info.alpha += (float)0.4 * Time.deltaTime;
+
+        }
+    }
+
+    public void closeInfo()
+    {
+        trans = true;
+    }
+}
