b4bba4bf3d4ec2115646c9ee09230086f7ad36a7
Lujia Yu <yulujia928@ufl.edu>
2018-04-06 14:52:46 -0400

    Sprint4

---
+++ Assets/Scripts/StoryModeButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryModeButton : MonoBehaviour {
+
+    public void storyMode()
+    {
+        Debug.Log("<b>Switch to Story Mode</b>");
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/Scripts/TransparencySlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransparencySlider : MonoBehaviour {
+    public Slider transparencySlider;
+    public GameObject head;
+    public Renderer headRenderer;
+	// Use this for initialization
+	void Start () {
+
+         head = GameObject.Find("head");
+
+        headRenderer = head.GetComponent<Renderer>();
+        transparencySlider.onValueChanged.AddListener(delegate { changeTrans(); });
+	}
+
+    public void changeTrans()
+    {
+         Color color = headRenderer.material.color;
+         color.a -= transparencySlider.value;
+         Debug.Log(color.a);
+         headRenderer.material.color = new Color(1, 1, 1, transparencySlider.value);
+        // Debug.Log(transparencySlider.value);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
